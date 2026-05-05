d18ed201136994432737650dc56212d84f436a4b
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-05 16:29:34 +0200

    Update stuctprojet.c

--- stuctprojet.c
+++ stuctprojet.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
+typedef struct {
+	int value;  //type de case
+	int player; //presence de joueur
+} _tile;
 
 typedef struct{
     int heure                    ;
