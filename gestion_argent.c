c0e6fa5b88b50b9a70984c3000fabbcc78b62f1f
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-12 15:59:29 +0200

    Update gestion_argent.c

--- gestion_argent.c
+++ gestion_argent.c
@@ -89,6 +89,32 @@ Jeu fonction_gestion_argent_cabinet(Jeu j) // j variable de type Jeu, d dentiste
     return j;
 }
 
+Jeu Gestion_argent_par_patient(Jeu j, Patient p){
+    FILE* fichier  = NULL ;
+
+    if (fichier == NULL){
+        printf("Ouverture du fichier impossible\n");
+        printf("code d'erreur = %d \n", errno );
+        printf("Message d'erreur = %s \n", strerror(errno) );
+        exit(1);
+    }
+
+    if (j.dentistes.gants == 0){
+        printf("Il faut mettre ses gants!");
+    }
+
+    else{
+        switch(){
+            
+
+          
+            default:
+                printf("Le client n'est vraiment pas content!");
+            break;
+         }
+    
+    retrun j;
+}
 
 int main(){
 Jeu argent; 
