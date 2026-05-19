0ebf53f92e8dc8d6a16a9641fdb71a2806b165db
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 13:38:35 +0200

    Update sauvegarde.c

--- sauvegarde.c
+++ sauvegarde.c
@@ -2,6 +2,47 @@
 #include <stdlib.h>
 #include <structprojet.h>
 
+void free_game(_jeu *jeu){
+	if (jeu == NULL) {
+		return;
+	}
+
+	// Libération des patients
+	if (jeu->plateau_tab != NULL){
+
+		for (int i = 0; i < jeu->nb_plateau; i++){
+
+			if (jeu->plateau_tab[i].patient != NULL){
+				free(jeu->plateau_tab[i].patient);
+				jeu->plateau_tab[i].patient = NULL;
+			}
+		}
+
+		free(jeu->plateau_tab);
+		jeu->plateau_tab = NULL;
+	}
+
+	// LibÃ©ration de la grille
+	if (jeu->grid != NULL) {
+
+	for (int y = 0; y < jeu->grid_size_y; y++) {
+
+		if (jeu->grid[y] != NULL) {
+			free(jeu->grid[y]);
+			}
+		}
+
+		free(jeu->grid);
+		jeu->grid = NULL;
+	}
+
+	jeu->nb_plateau = 0;
+	jeu->grid_size_x = 0;
+	jeu->grid_size_y = 0;
+} 
+
+
+
 void save_game(_jeu *jeu, const char *filename){
 	printf("%d octet \n",sizeof(*jeu));
 	FILE *f = fopen(filename, "wb");
@@ -151,6 +192,8 @@ void save_game(_jeu *jeu, const char *filename){
 	fclose(f);
 	printf("Partie sauvegardee avec succes !\n");
 }
+
+
 
 void load_game(_jeu *jeu, const char *filename)
 {
