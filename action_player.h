21761017601f879b2f3b08ff3d4a210f098b8073
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 16:32:40 +0200

    Update action_player.h

--- action_player.h
+++ action_player.h
@@ -1,8 +1,7 @@
-#ifndef ACTION_PLAYER.H
-  #define ACTION_PLAYER.H
+#ifndef ACTION_PLAYER_H
+  #define ACTION_PLAYER_H
 
-#include <stdio.h>
-#include <structproject.h>
+#include "structproject.h"
 
 char try_do_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab ,int taille ,float* profit ,int* hummeur_tab){  
 void ask_to_do_player_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab,int taille ,float* profit ,int* hummeur_tab ,int* playing) {
