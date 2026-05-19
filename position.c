ccf18c61fae9d8841dbe51e699cc11dcf3a7e185
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 14:32:44 +0200

    Update position.c

--- position.c
+++ position.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<unistd.h>
+
+#include <string.h>
+#include <errno.h> 
+
+#define GRID_SIZE_X 20
+#define GRID_SIZE_Y 10
+
+#define NB_TOOLS 7  //sans le gant
+#define NB_MALADIE 5 //sans le test_desease
+#define NB_hummeur 3
+#define MAX_map_string 5000
+#define username_SIZE 50
+
 
