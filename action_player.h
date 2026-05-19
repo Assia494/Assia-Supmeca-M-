f1ad051928dfa9c92ee6efe744ccd2ed26b6028f
Assia494 <assialarabi.07@gmail.com>
2026-05-19 15:26:43 +0200

    Fix function calls in action_player.h

--- action_player.h
+++ action_player.h
@@ -1,6 +1,11 @@
+#ifndef ACTION_PLAYER.H
+  #define ACTION_PLAYER.H
 
-
-
+#include <stdio.h>
+#include <structproject.h>
 
 try_do_action(); 
-ask_do_action(); 
+ask_to_do_player_action(); 
+
+
+#endif
