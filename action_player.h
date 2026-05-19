bbc9092a578883773e28bd31b2194b2ded5f804f
Assia494 <assialarabi.07@gmail.com>
2026-05-19 15:28:57 +0200

    Update action_player.h

--- action_player.h
+++ action_player.h
@@ -4,8 +4,7 @@
 #include <stdio.h>
 #include <structproject.h>
 
-try_do_action(); 
-ask_to_do_player_action(); 
-
+char try_do_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab ,int taille ,float* profit ,int* hummeur_tab){  
+void ask_to_do_player_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab,int taille ,float* profit ,int* hummeur_tab ,int* playing) {
 
 #endif
