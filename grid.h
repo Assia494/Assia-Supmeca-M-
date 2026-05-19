11f487f8a67f44e0b3f3ceb04ec0be6cfdf6ddc2
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 15:25:25 +0200

    Update grid.h

--- grid.h
+++ grid.h
@@ -11,5 +11,7 @@ _coord get_element_pos_from_grid(_tile** grid, int size_x, int size_y, int eleme
 
 _tile get_tile_from_pos(_tile** grid, int size_x, int size_y, int x, int y);
 int can_move_at_pos(_tile** grid, int size_x, int size_y, int x, int y);
+void move_player(_tile** grid,int size_x,int size_y,_movement movement)
+_tile** make_grid_from_string(char string[] ,int max_size_x ,int max_size_y)
 
 #endif
