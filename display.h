0e9f3de8bfedbdb2584037c0f94fd9534a1ff6f4
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 15:14:23 +0200

    Update display.h

--- display.h
+++ display.h
@@ -1,5 +1,8 @@
 #ifndef DISPLAY_H
-#define DISPLAY_H
+  #define DISPLAY_H
+
+#include <fonctionutile.h>
+#include <structprojet.h>
 
 void tile_print(_tile tile, _plateau* plateau_tab, int taille, _player player);
 void print_grid(_tile** grid, int size_x, int size_y,
