e87fccd31a515287efe7d9a535d8e6214f69d1e9
Assia494 <assialarabi.07@gmail.com>
2026-05-19 15:20:51 +0200

    Update display.h

--- display.h
+++ display.h
@@ -5,7 +5,11 @@
 #include <structprojet.h>
 
 void tile_print(_tile tile, _plateau* plateau_tab, int taille, _player player);
-void print_grid(_tile** grid, int size_x, int size_y,
-_plateau* plateau_tab, int taille, _player player);
+void print_grid(_tile** grid, int size_x, int size_y,_plateau* plateau_tab, int taille, _player player);
+printf_red_green(); 
+print_player_status(); 
+print_total_patient(); 
+print_plateau(); 
+print_plateau_tab(); 
 
 #endif
