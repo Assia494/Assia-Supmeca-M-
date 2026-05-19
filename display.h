c788a51691310210e0738c821d3f8a731fd3ea61
Assia494 <assialarabi.07@gmail.com>
2026-05-19 14:49:05 +0200

    Update display.h

--- display.h
+++ display.h
@@ -2,7 +2,6 @@
   #define DISPLAY_H
 
 void tile_print(_tile tile, _plateau* plateau_tab, int taille, _player player);
-void print_grid(_tile** grid, int size_x, int size_y,
- _plateau* plateau_tab, int taille, _player player);
+void print_grid(_tile** grid, int size_x, int size_y, _plateau* plateau_tab, int taille, _player player);
 
 #endif
