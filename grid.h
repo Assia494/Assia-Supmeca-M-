06420111db0402d7faafb9cda36b84b2def2b274
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 14:51:11 +0200

    Create grid.h

---
+++ grid.h
@@ -0,0 +1,13 @@
+#ifndef GRID_H
+#define GRID_H
+
+_tile** cree_grid(int size_x, int size_y);
+_tile cree_tile();
+
+_coord get_player_pos_from_grid(_tile** grid, int size_x, int size_y);
+_coord get_element_pos_from_grid(_tile** grid, int size_x, int size_y, int element);
+
+_tile get_tile_from_pos(_tile** grid, int size_x, int size_y, int x, int y);
+int can_move_at_pos(_tile** grid, int size_x, int size_y, int x, int y);
+
+#endif
