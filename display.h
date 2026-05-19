ee9c057af6369018be18bd82219d276556c18e54
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 16:00:04 +0200

    Update display.h

--- display.h
+++ display.h
@@ -7,7 +7,8 @@
 #include <errno.h>
 #include <time.h>
 
-#include <strutprojet.h>
+#include "structprojet.h"
+#include "fonctionutile.h"
 
 void tile_print(_tile tile, _plateau* plateau_tab, int taille, _player player);
 void print_grid(_tile** grid, int size_x, int size_y,_plateau* plateau_tab, int taille, _player player);
