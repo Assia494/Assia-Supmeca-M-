2e5d4b6156818abbc4261d15b0e29db1d4816acb
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 14:30:50 +0200

    Update creer.h

--- creer.h
+++ creer.h
@@ -7,11 +7,12 @@
 #include <fonctionutile.h>
 
 
-_tile cree_tile();
-_tile** cree_grid(int size_x,int size_y);
-_plateau cree_plateau(int new_id);
-void make_tool_tab(int tab[] ,int a,int b,int c,int d,int e,int f,int g);
+_tile     cree_tile();
+_tile**   cree_grid(int size_x,int size_y);
+_plateau  cree_plateau(int new_id);
+void      make_tool_tab(int tab[] ,int a,int b,int c,int d,int e,int f,int g);
 _patient* cree_patient();
-_jeu creer_jeu();
+_jeu      creer_jeu();
+int*      cree_tab(int size);
 
 #endif
