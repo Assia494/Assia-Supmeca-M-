110f92c2905988e46216cb08da498193b0b66f3c
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 17:15:40 +0200

    Update base.h

--- base.h
+++ base.h
@@ -3,7 +3,7 @@
 
 #include "strutprojet.h"
 #include "fonctionutile.h"
-
+#include "main.h"
 _jeu creer_jeu();
 int play_a_game(_jeu* game ,char* username); 
 _menu ask_menu(_jeu* current_game ,_menu current_menu) ; 
