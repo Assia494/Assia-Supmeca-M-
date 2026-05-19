06c1c9769f8f4878bb09e67a168f6b13d00ffa56
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 14:43:35 +0200

    Update fonctionutile.h

--- fonctionutile.h
+++ fonctionutile.h
@@ -1,15 +1,11 @@
-#ifndef FONCTIONUTILE_H
-	#define FONCTIONUTILE_H
+#ifndef UTILS_H
+	#define UTILS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <structprojet.h>
+int randint(int a, int b);
+int inter_check(int nb, int a, int b);
+void exit_if_null_pointer(void* pointer);
 
 void color(unsigned char r, unsigned char g, unsigned char b);
-void reset_color(); 
-int randint(int a,int b);
-int inter_check(int nb,int a,int b);
-void exit_if_null_pointer(void* pointer);
-_tile cree_tile();
+void reset_color(void);
 
 #endif
