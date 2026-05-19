1da103f21927c4b0be0b78d93810a1c1ef875ce0
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 15:36:08 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,20 @@
+#ifndef  BASE.H
+  #define BASE.H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+
+#include <strutprojet.h>
 #include <fonctionutile.h>
 #include <grid.h>
 #include <game.h>
 #include <display.h>
 #include <economi.h>
 #include <patient.h>
+#include <base.h>
+#include <plateau.h>
+
+#endif
