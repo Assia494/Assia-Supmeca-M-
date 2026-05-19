146ba868990d21aa07eb050beb511595d8527c52
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 13:55:58 +0200

    Update fonctionutile.h

--- fonctionutile.h
+++ fonctionutile.h
@@ -5,3 +5,11 @@
 #include <stdlib.h>
 #include <structprojet.h>
 
+void color(unsigned char r, unsigned char g, unsigned char b);
+void reset_color(); 
+int randint(int a,int b);
+int inter_check(int nb,int a,int b);
+void exit_if_null_pointer(void* pointer);
+_tile cree_tile();
+
+#enddef
