ba185f8fd92c81d9da47031b21cffd668df1a88d
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 14:08:26 +0200

    Update PROJET.c

--- PROJET.c
+++ PROJET.c
@@ -6,8 +6,8 @@
 #include <string.h>
 #include <errno.h> 
 
-//#define GRID_SIZE_X 20
-//#define GRID_SIZE_Y 10
+#define GRID_SIZE_X 20
+#define GRID_SIZE_Y 10
 
 #define NB_TOOLS 7  //sans le gant
 #define NB_MALADIE 5 //sans le test_desease
