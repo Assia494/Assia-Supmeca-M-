ea4deaff4ba768bebc912d5161e6cf798aa3d4fb
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 13:49:39 +0200

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -9,5 +9,7 @@ void save_game(_jeu *jeu, const char *filename);
 void load_game(_jeu *jeu, const char *filename);
 void free_game(_jeu *jeu);
 void exit_if_null_pointer(void* pointer);
+_coord get_player_pos_from_grid(_tile** grid,int size_x,int size_y);
+
 
 #endif
