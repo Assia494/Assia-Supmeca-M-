ab71ba8c44ba4f3ee19abb0181d61d23eadbaa23
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 15:32:21 +0200

    Update plateau.h

--- plateau.h
+++ plateau.h
@@ -2,7 +2,7 @@
 
 
 
-cree_plateau(); 
+_plateau cree_plateau(int new_id); 
 cure_if_got_tools(); 
 try_cure_patient(); 
-get_plateau_tab(); 
+_plateau* get_plateau_tab(_tile** grid,int size_x,int size_y,int* taille); 
