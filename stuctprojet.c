4d0fc1846d0eb98ce96f9330c449b812b8ea2dba
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 13:00:53 +0200

    Update stuctprojet.c

--- stuctprojet.c
+++ stuctprojet.c
@@ -1,54 +1,88 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
+#define NB_TOOLS 7
+#define NB_MALADIE 5
+#define NB_hummeur 3
+#define MAX_map_string 5000
+ #define username_SIZE 50
+//-----------------------------------------------------------
+typedef enum {UP=0,RIGHT=1,DOWN=2,LEFT=3} _movement;
+//-----------------
+typedef enum {TEST_DESEASE=0,DESEASE_A=1,DESEASE_B=2,DESEASE_C=3,DESEASE_D=4,DESEASE_E=5} _maladie_type;
+//-----------------
+typedef enum {satisfait=0,mecontant=1,furieux=2} _etat;
+//-----------------
+typedef enum {new_jeu=0,select_menu=1,scoreboard=2,save=3,continu=4,quit=5} _menu;
+//-----------------
 typedef struct {
-	int value;  //type de case
-	int player; //presence de joueur
+    int value;
+    int player;
 } _tile;
-
-typedef struct{
-    int heure                    ;
-    int minute                   ;
-    int seconde                  ;
-} Temps; 
-
-typedef struct{    
-    Dentiste  dentiste           ; // on peut augmenter au bout d'un montant
-    Temps     temps_debut_partie ;   
-    Patient   patient[7]         ; 
-    int       nb_client          ; // depuis le debut de la parti
-    float     argent_cabinet     ; // ensemble argent gagne
-    int       humeur[3]          ; //nb patient mecontent, content et furieux
-} Jeu;
-
+//-----------------
 typedef struct {
-	int x;
-	int y;
+    int x;
+    int y;
 } _coord;
-
-typedef enum{
-    Satisfait;
-    Mecontent;
-    Furieux;
-} EtatPatient;
-
-
-typedef struct{
-    _coord position               ; // du dentiste sur plateau
-    int    gants                  ; // mis = 1 pas mis = 0
-    int    ustensile_en_main      ; // num ustensile
-    int    main                   ; // pleine = 1 pas pleine = 0   
-    bool   proprete_ustensile     ; // true=propre false=sale 
-    bool   proprete_gants         ;        //
-} Dentiste;
-
-typedef struct{
-    int    plateau[7]              ; // tableau avec num ustensiles
-    int    ustensile            ; // 1 ustensile = 1 nombre
-    int    pathologie           ; // 1 pathologie = 1 nombre
-    float  montant_a_payer      ; // diminue si qualité mauvaise
-    Humeur humeur            ; // jauge
-    int    fauteuil             ; //vide ou non
-    Temps  heure_arrive         ; // a partir du moment où il est dans le  fauteuil
-    EtatPatient etat            ; //mecontent, furieux, heureux
-} Patient;
+//-----------------
+typedef struct {
+    int clean;
+    int used;
+    int type;
+} _tool;
+//-----------------
+typedef struct {
+    _coord pos;
+    _tool tool;
+    _tool glove;
+} _player;
+//-----------------
+typedef struct {
+    _maladie_type type;
+    int tool_needed[NB_TOOLS];
+    float profit;
+} _maladie;
+//-----------------
+typedef struct {
+    int hummeur;
+    _maladie maladie;
+    _etat etat;
+} _patient;
+//-----------------
+typedef struct {
+    int tools[NB_TOOLS];
+    int used_tools[NB_TOOLS];
+    _patient* patient;
+    int id;
+} _plateau;
+//-----------------
+typedef struct {
+    int nb_step;
+    int play;
+    int hummeur_tab[NB_hummeur];
+    _player player;
+    float profit;
+    int grid_size_x;
+    int grid_size_y;
+    _tile** grid;
+    int happy_bar_len;
+    int nb_plateau;
+    _plateau* plateau_tab;
+    int patient_minimum_spawn_intervalle;
+    int patient_spawn_range;
+    int patient_spawning_hapiness;
+    int patient_hapiness_range;
+    int next_patient_time;
+    char username[username_SIZE];
+} _jeu;
+//-----------------
+typedef struct {
+    char username[50];
+    int nb_step;
+    float profit;
+    int hummeur_tab[NB_hummeur];
+} _score;
