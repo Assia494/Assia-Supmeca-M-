13f07b21b7711105d7b0d8d8bc5bfc1de91e2f40
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 13:40:17 +0200

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -1,4 +1,5 @@
-#ifndef 
+#ifndef SAUVEGARDE_H
+	#define SAUVEGARDE_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,4 +8,3 @@
 void save_game(_jeu *jeu, const char *filename);	
 void load_game(_jeu *jeu, const char *filename);
 void free_game(_jeu *jeu);
-	
