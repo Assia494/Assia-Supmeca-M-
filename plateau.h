0fc202eba3b881f17f0ee249a51bfc20e982da66
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 15:38:57 +0200

    Update plateau.h

--- plateau.h
+++ plateau.h
@@ -1,4 +1,13 @@
+#ifndef  BASE.H
+  #define BASE.H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+
+#include <strutprojet.h>
 
 
 
@@ -6,3 +15,5 @@ _plateau cree_plateau(int new_id);
 int cure_if_got_tools(_plateau* plateau ,float* profit ,int* hummeur_tab); 
 int try_cure_patient(_plateau* plateau ,float* profit ,int* hummeur); 
 _plateau* get_plateau_tab(_tile** grid,int size_x,int size_y,int* taille); 
+
+#endif
