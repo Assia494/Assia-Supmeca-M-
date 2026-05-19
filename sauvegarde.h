709733586ce15b279f13ed23a16cf34c488eb3c2
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 13:50:19 +0200

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <structprojet.h>
+#include <fonctionutile.h>
 
 void save_game(_jeu *jeu, const char *filename);	
 void load_game(_jeu *jeu, const char *filename);
