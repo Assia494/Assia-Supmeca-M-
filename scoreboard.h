9bc4ec8cf3c2ec130feb53a9cbf4272c6f489915
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 15:36:58 +0200

    Update scoreboard.h

--- scoreboard.h
+++ scoreboard.h
@@ -1,8 +1,16 @@
+#ifndef  BASE.H
+  #define BASE.H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
 
-
-
+#include <strutprojet.h>
 
 score_swap();
 _score_bubble_sort();
 print_scoreboard(); 
+
+#endif
