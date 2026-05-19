5c17390035e6381be5136d7dff02573e4f8029f8
Assia494 <assialarabi.07@gmail.com>
2026-05-19 16:31:54 +0200

    Update base.h

--- base.h
+++ base.h
@@ -1,15 +1,8 @@
 #ifndef  BASE.H
   #define BASE.H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <time.h>
-
-#include <strutprojet.h>
-
-
+#include "strutprojet.h"
+#include "fonctionutile"
 
 _jeu creer_jeu();
 int play_a_game(_jeu* game ,char* username); 
