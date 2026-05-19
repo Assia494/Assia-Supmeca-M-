cbbfb888145a1aa55243812f0dc503cd0d58cde2
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-19 14:18:11 +0200

    Update creer.c

--- creer.c
+++ creer.c
@@ -1,1 +1,8 @@
+_tile cree_tile() {                                              //creer une case de la grille avec des données par défaut                                    
+	_tile new_tile;  
+	new_tile.value = 0;
+	new_tile.player = 0;
+	return new_tile;
+}
+
 
