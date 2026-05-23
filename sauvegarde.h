1c0027b56cd34103925e3de9023ebb79aebf5f88
Assia494 <assialarabi.07@gmail.com>
2026-05-23 15:34:19 +0200

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -4,6 +4,11 @@
 #include "structprojet.h"
 #include "fonctionutile.h"
 #include "grid.h"
+#include "plateau.h"
+#include "patient.h"
+#include "economy.h"
+
+
 
 void save_game(const _jeu *jeu, const char *filename);
 void load_game(_jeu *jeu, const char *filename);
