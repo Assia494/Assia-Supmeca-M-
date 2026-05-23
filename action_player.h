9f49dbf97b0519372d503bb5a10a2eb4a238180b
Assia494 <assialarabi.07@gmail.com>
2026-05-23 15:12:07 +0200

    Update action_player.h

--- action_player.h
+++ action_player.h
@@ -2,10 +2,17 @@
   #define ACTION_PLAYER_H
 
 #include "structprojet.h"
+#include "fonctionutile.h"
 #include "grid.h"
-#include "plateau.h"
 #include "game.h"
-
+#include "display.h"
+#include "economy.h"
+#include "patient.h"
+#include "base.h"
+#include "plateau.h"
+#include "creer.h"
+#include "scoreboard.h"
+#include "action_player.h"
 
 char try_do_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab ,int taille ,float* profit ,int* hummeur_tab);  
 void ask_to_do_player_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab,int taille ,float* profit ,int* hummeur_tab ,int* playing);
