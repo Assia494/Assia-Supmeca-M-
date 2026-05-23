7cfacfedb929e92b1566a8c6f78f29b320345421
Assia494 <assialarabi.07@gmail.com>
2026-05-23 20:42:04 +0200

    Update action_player.h

--- action_player.h
+++ action_player.h
@@ -13,7 +13,7 @@
 #include "creer.h"
 #include "action_player.h"
 
-char try_do_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab ,int taille ,float* profit ,int* hummeur_tab);  
-void ask_to_do_player_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab,int taille ,float* profit ,int* hummeur_tab ,int* playing);
+char try_do_action(_tile** grid, int size_x, int size_y, _player* player, _plateau* plateau_tab, int taille, float* profit, int* hummeur_tab, int* game_over); 
+void ask_to_do_player_action(_tile** grid, int size_x, int size_y, _player* player, _plateau* plateau_tab, int taille, float* profit, int* hummeur_tab, int* playing, int* game_over);
 
 #endif
