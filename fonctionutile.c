152546c6c44c2f78d0ce325507babcbd7b73e56a
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-05-23 15:41:07 +0200

    Update fonctionutile.c

--- fonctionutile.c
+++ fonctionutile.c
@@ -50,3 +50,17 @@ void decrease_if_to_much(int* var ,int min){//(fonction pas utile)  diminuer si 
         *var--;
     }    
 }
+
+
+
+int* cree_tab(int size){//cree un tableau 
+    //(temporaire)
+    int* tab =NULL;
+    tab = malloc(size*sizeof(int));
+    exit_if_null_pointer(tab);
+    for(int i=0;i<size;i++){
+        tab[i] = randint(100,10000);  //initialisation des valeurs du tableau cree 
+    }
+    
+    return tab;
+}
