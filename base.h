6365124ca7d8bee13b3d37064d2f357b42468e6a
Assia494 <assialarabi.07@gmail.com>
2026-05-23 19:51:48 +0200

    Update base.h

--- base.h
+++ base.h
@@ -12,7 +12,7 @@
 
 _jeu creer_jeu();
 int play_a_game(_jeu* game ,char* username); 
-_menu ask_menu(_jeu* current_game ,_menu current_menu) ; 
+_menu ask_menu(_jeu* current_game, _menu current_menu, const char* filename);
 void start();
 
 #endif
