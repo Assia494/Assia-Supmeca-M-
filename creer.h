11c19c8d9292f2454121ada7709a5cd532960a27
Assia494 <assialarabi.07@gmail.com>
2026-05-23 20:52:07 +0200

    Update creer.h

--- creer.h
+++ creer.h
@@ -1,5 +1,5 @@
-#ifndef  CREER_H
-  #define CREER_H
+#ifndef CREER_H
+#define CREER_H
 
 #include "structprojet.h"
 #include "fonctionutile.h"
@@ -7,13 +7,14 @@
 #include "grid.h"
 #include "plateau.h"
 
-_tile** cree_grid(int size_x, int size_y);
-_tile cree_tile();
+// Ajout de static inline pour tricher avec le Linker sans modifier les fichiers .c
+static inline _tile** cree_grid(int size_x, int size_y);
+static inline _tile cree_tile();
 
-_coord get_player_pos_from_grid(_tile** grid, int size_x, int size_y);
-_coord get_element_pos_from_grid(_tile** grid, int size_x, int size_y, int element);
+static inline _coord get_player_pos_from_grid(_tile** grid, int size_x, int size_y);
+static inline _coord get_element_pos_from_grid(_tile** grid, int size_x, int size_y, int element);
 
-_tile get_tile_from_pos(_tile** grid, int size_x, int size_y, int x, int y);
-int can_move_at_pos(_tile** grid, int size_x, int size_y, int x, int y);
+static inline _tile get_tile_from_pos(_tile** grid, int size_x, int size_y, int x, int y);
+static inline int can_move_at_pos(_tile** grid, int size_x, int size_y, int x, int y);
 
 #endif
