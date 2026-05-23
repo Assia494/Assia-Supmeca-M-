9c6ad99ebf3729a923cc696e3ebbe1a1d43b57ff
Assia494 <assialarabi.07@gmail.com>
2026-05-23 20:55:50 +0200

    Update creer.h

--- creer.h
+++ creer.h
@@ -7,14 +7,13 @@
 #include "grid.h"
 #include "plateau.h"
 
-// Ajout de static inline pour tricher avec le Linker sans modifier les fichiers .c
-static inline _tile** cree_grid(int size_x, int size_y);
-static inline _tile cree_tile();
+_tile** cree_grid(int size_x, int size_y);
+_tile cree_tile();
 
-static inline _coord get_player_pos_from_grid(_tile** grid, int size_x, int size_y);
-static inline _coord get_element_pos_from_grid(_tile** grid, int size_x, int size_y, int element);
+_coord get_player_pos_from_grid(_tile** grid, int size_x, int size_y);
+_coord get_element_pos_from_grid(_tile** grid, int size_x, int size_y, int element);
 
-static inline _tile get_tile_from_pos(_tile** grid, int size_x, int size_y, int x, int y);
-static inline int can_move_at_pos(_tile** grid, int size_x, int size_y, int x, int y);
+_tile get_tile_from_pos(_tile** grid, int size_x, int size_y, int x, int y);
+int can_move_at_pos(_tile** grid, int size_x, int size_y, int x, int y);
 
 #endif
