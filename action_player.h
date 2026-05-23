8eb9876c2ea92d7a28f0329616fffc41ba5de3bc
Assia494 <assialarabi.07@gmail.com>
2026-05-23 15:09:51 +0200

    Fix include guards in action_player.h

--- action_player.h
+++ action_player.h
@@ -6,6 +6,7 @@
 #include "plateau.h"
 #include "game.h"
 
+
 char try_do_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab ,int taille ,float* profit ,int* hummeur_tab);  
 void ask_to_do_player_action(_tile** grid,int size_x,int size_y,_player* player ,_plateau* plateau_tab,int taille ,float* profit ,int* hummeur_tab ,int* playing);
 
