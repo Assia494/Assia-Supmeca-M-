a99d00ba5160d81762f9e56342511af588b28b96
Assia494 <assialarabi.07@gmail.com>
2026-05-24 17:45:55 +0200

    Update fonctionutile.c

--- fonctionutile.c
+++ fonctionutile.c
@@ -4,7 +4,16 @@ void color(unsigned char r, unsigned char g, unsigned char b){    //couleur affi
 	printf("\x1B[38;2;%d;%d;%dm", r, g, b);                       // red, green ,blue
 }
 
-
+void make_tool_tab(int tab[] ,int a,int b,int c,int d,int e,int f,int g){
+    tab[0] = a;
+    tab[1] = b;
+    tab[2] = c;
+    tab[3] = d;
+    tab[4] = e;
+    tab[5] = f;
+    tab[6] = g;
+}  //remplir un tableau d'outils nécessaire à partir des paramètres  a b c d e f g
+//-----------------------------------------------------------
 
 
 void reset_color(){
