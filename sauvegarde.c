0283c07d727512db8d75fcfdf9eaa3c5e3f875b5
Assia494 <assialarabi.07@gmail.com>
2026-05-24 17:08:36 +0200

    Update sauvegarde.c

--- sauvegarde.c
+++ sauvegarde.c
@@ -38,26 +38,9 @@ void free_game(_jeu *jeu){
 	jeu->grid_size_x = 0;
 	jeu->grid_size_y = 0;
 } 
-//-----------------------------------------------------------
-_tile** cree_grid(int size_x,int size_y) {                       //crée un tableau case 2D   ,pour la grille de jeu
 
-	_tile** new_grid = NULL;
-	new_grid = malloc( size_y * sizeof(_tile*) );
-	exit_if_null_pointer(new_grid);
 
-	for(int dy=0 ; dy<size_y ; dy++) {
-		new_grid[dy] = malloc( size_x * sizeof(_tile) );
-		exit_if_null_pointer(new_grid[dy]);
 
-		for(int dx=0 ; dx<size_x ; dx++) {
-			new_grid[dy][dx] = cree_tile();
-			new_grid[dy][dx].value = 0;
-		}
-	}
-	return new_grid;
-
-}
-//-----------------------------------------------------------
 void save_game(_jeu *jeu, const char* filename){
 	//printf("%d octet \n",sizeof(jeu));
 	
